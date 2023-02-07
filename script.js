cb95b29a419452910eea2c67df7fb41c38c38c7f
rockere93 <pirat93.93@mail.ru>
2023-02-07 10:29:55 +0500

    added set for message.text

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 'use strict'
+let textMessage = document.querySelector('.textmessage')
 let buttonsBlock = document.querySelector('.buttons');
 
 let player = {
-    name,
-    gender: " ",
+    name: '',
+    gender: '',
     inventory: [],
 }
 
 
 let story = {
+    goToMessage (message) {
+        textMessage.innerHTML = ' ' + `${message.text}`;
+    
+    },
+
     message0: {
         text: 'Приветствую тебя, добрый молодец! Али ты девица красная? Проходи, не стесняйся, о себе расскажи, не тушайся. Мы тута всем рады, для каждого и хлеб, и соль найдется, а коли не найдется так чем богаты, тем и рады будем.',
         actions: {chooseSex (sex) {
             player.gender = sex;
-            console.log(player.gender)
+            story.goToMessage(story.message1);
+            console.log(player.gender)           
         }
     },
 
     },
     message1: {
-        text: `${player.gender}В общем, здесь на лубочно-клюквенном языке мы и закончим. Это тебе не сказка и даже не быль. Это кошмар, в котором тебе не повезло оказаться.`
+        get text () { return `${player.gender}, ну что ж. В общем, здесь на лубочно-клюквенном языке мы и закончим. Это тебе не сказка и даже не быль. Это кошмар, в котором тебе не повезло оказаться.`}
     },
     message2: {
         text: "Ты находишь в темной бревенчатой комнате, единственный свет – из узкого, как бойница, окна, через который видно краешек серой хмари. В нос бьет сильный запах плесени и влажного мха, который утыкан между старыми потемневшими брёвнами. По центру стоит печь, достаточно большая, чтобы туда поместился человек, но чья кладка рассыпается как будто прямо на глазах, обнажая тёмное и пугающее нутро. Также видна дверь, к которой можно подойти.",
         actions: {
             lookAround () {
-
+              
+                
             },
             comeToDoor () {
 
@@ -36,9 +44,8 @@ let story = {
             },
         }
     }
-
-
 }
 
-
+story.message2.actions.lookAround()
+console.log(story.message2.actions.lookAround._name);
 
