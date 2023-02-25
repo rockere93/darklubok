cdfe4d8903d7dd8bf09ebcbf4e5937b92b16a992
rockere93 <124269776+rockere93@users.noreply.github.com>
2023-02-25 18:21:12 +0500

    update intro
    - all storyCards have been moved in array intro,
    - corrected func goToStoryCard for work with array

--- story/intro/intro.js
+++ story/intro/intro.js
@@ -1,80 +1,81 @@
 'use strict'
+const intro = [
+    {
+        name: 'introCard0',
+        get text() {
+            return `Приветствую тебя, добрый молодец! Али ты девица красная? Подслеповат я к старости стал, уж не серчай на старика.`
+        },
+        buttons: [
+            {
+                nameButton: 'Добрый молодец',
+                functionButton: chooseSex,
+                arg: ['Добрый молодец',],
+            },
+            {
+                nameButton: 'Красна девица',
+                functionButton: chooseSex,
+                arg: ['Красна девица',],
+            },
+        ]
+    },
+    {
+        name: 'introCard1',
+        get text() {
+            return `${player.gender} значит. Ну а зовут-то тебя как?`
+        },
+        buttons: [
+            {
+                nameButton: 'Подтвердить',
+                functionButton: agreeName,
+            },
+        ]
+    },
+
+
+    {
+        name: 'introCard2',
+        get text() {
+            return `${player.name}, ну что ж. В общем, здесь на лубочно-клюквенном языке мы и закончим. Это тебе не сказка и даже не быль. Это кошмар, в котором тебе не повезло оказаться.`
+        },
+        buttons: [
+            {
+                nameButton: 'Продолжить',
+                functionButton: goToNextChapter,
+            },
+        ]
+    },
+];
+
 function chooseSex(gender) {
     player.gender = gender;
-    goToStoryCard(introCard1);
-    addInputName ();
+    goToStoryCard(intro, 1);
+    addInputName();
 }
 
-function addInputName () {
+function addInputName() {
     let input = document.createElement('input');
     input.id = "inputName";
     input.placeholder = "Нажмите сюда, чтобы ввести имя";
     buttonsBlock.prepend(input);
 }
 
-function agreeName () {
+function agreeName() {
     let input = buttonsBlock.querySelector('input');
     player.name = input.value;
-    goToStoryCard(introCard2);
+    goToStoryCard(intro, 2);
     animationDark();
 }
 
 
-function animationDark () {
+function animationDark() {
     if (!mainField.classList.contains('darkAnimation')) {
-    setTimeout(() => mainField.classList.add('darkAnimation'), 2000)
+        setTimeout(() => mainField.classList.add('darkAnimation'), 2000)
     }
 }
 
-function goToNextChapter () {
-    dowloadChapterScript ('story/chapter1/chapter1.js', 'Глава 1');
+function goToNextChapter() {
+    dowloadChapterScript('story/chapter1/chapter1.js', 'Глава 1');
 }
 
-function buttonsfi () {
-    alert("затычка")}
-
-const introCard0 = {
-    get text() { 
-        return `Приветствую тебя, добрый молодец! Али ты девица красная? Подслеповат я к старости стал, уж не серчай на старика.`},
-    buttons: [
-        {
-            nameButton: 'Добрый молодец',
-            functionButton: chooseSex,
-            arg: ['Добрый молодец',],
-        },
-        {
-            nameButton: 'Красна девица',
-            functionButton: chooseSex,
-            arg: ['Красна девица',],
-        },
-    ]
-}
-
-
-const introCard1 = {
-    get text() { 
-        return `${player.gender} значит. Ну а зовут-то тебя как?`},
-    buttons: [
-        {
-            nameButton: 'Подтвердить',
-            functionButton: agreeName,
-        },
-    ]
-};
-
-
-
-const introCard2 = {
-    get text() { 
-        return `${player.name}, ну что ж. В общем, здесь на лубочно-клюквенном языке мы и закончим. Это тебе не сказка и даже не быль. Это кошмар, в котором тебе не повезло оказаться.` },
-    buttons: [
-        {
-            nameButton: 'Продолжить',
-            functionButton: goToNextChapter,
-        },
-    ]
-};
-
-
-goToStoryCard(introCard0);
+goToStoryCard(intro, 0);
 setTimeout(() => mainFieldBody.classList.add('animationText'), 1000)
