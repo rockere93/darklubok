cf3e3cad89ff4e33ac791e06711f5005a342231d
rockere93 <pirat93.93@mail.ru>
2023-06-21 14:33:34 +0500

    feat: add scrollbar for text in battle

--- src/scripts/DOM-elements/mainelements.js
+++ src/scripts/DOM-elements/mainelements.js
@@ -3,5 +3,6 @@ const buttonsBlock = document.querySelector('.buttons_block');
 const mainfield = document.querySelector('.mainfield');
 const textCard = document.querySelector('.textCard');
 const mainfieldBody = document.querySelector('.mainfield__body');
+const viewport = document.querySelector('.viewport');
 
-export { pageBody, buttonsBlock, mainfield, textCard, mainfieldBody };
+export { pageBody, buttonsBlock, mainfield, textCard, mainfieldBody, viewport };
